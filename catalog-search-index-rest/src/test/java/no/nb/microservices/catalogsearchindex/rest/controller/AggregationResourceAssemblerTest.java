0796f35972874fb3eb9f1e3a9dd13c8322977e53
alfredkw <alfred.wahle@nb.no>
2015-11-18 12:06:49 +0100

    BT-3524 api: geosøk

--- catalog-search-index-rest/src/test/java/no/nb/microservices/catalogsearchindex/rest/controller/AggregationResourceAssemblerTest.java
+++ catalog-search-index-rest/src/test/java/no/nb/microservices/catalogsearchindex/rest/controller/AggregationResourceAssemblerTest.java
@@ -1,10 +1,9 @@
 package no.nb.microservices.catalogsearchindex.rest.controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.elasticsearch.search.aggregations.Aggregation;
+import no.nb.microservices.catalogsearchindex.AggregationResource;
+import org.elasticsearch.common.geo.GeoPoint;
+import org.elasticsearch.search.aggregations.bucket.geogrid.GeoHashGrid;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,9 +11,18 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import no.nb.microservices.catalogsearchindex.AggregationResource;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AggregationResourceAssemblerTest {
+
+    private AggregationResourceAssembler assembler;
 
     @Before
     public void init() {
@@ -22,6 +30,7 @@ public class AggregationResourceAssemblerTest {
         ServletRequestAttributes attributes = new ServletRequestAttributes(request);
 
         RequestContextHolder.setRequestAttributes(attributes);
+        assembler = new AggregationResourceAssembler();
     }
     
     @After
@@ -31,12 +40,47 @@ public class AggregationResourceAssemblerTest {
     
     @Test
     public void testToResource() {
-        AggregationResourceAssembler assembler = new AggregationResourceAssembler();
-        Aggregation mockAggregation = mock(Aggregation.class);
-        when(mockAggregation.getName()).thenReturn("ddc1");
-        
+        Terms mockAggregation = createMockedTermsAggregation();
+
         AggregationResource resource = assembler.toResource(mockAggregation);
         
         assertEquals("ddc1", resource.getName());
+        assertThat(resource.getFacetValues(), hasSize(1));
+    }
+
+    private Terms createMockedTermsAggregation() {
+        List<Terms.Bucket> buckets = new ArrayList<>();
+        Terms.Bucket mockBucket = mock(Terms.Bucket.class);
+        when(mockBucket.getKey()).thenReturn("bucket");
+        when(mockBucket.getDocCount()).thenReturn(1L);
+        buckets.add(mockBucket);
+
+        Terms mockAggregation = mock(Terms.class);
+        when(mockAggregation.getName()).thenReturn("ddc1");
+        when(mockAggregation.getBuckets()).thenReturn(buckets);
+        return mockAggregation;
+    }
+
+    @Test
+    public void testToResourceWithGeoHashGridAggregation() {
+        GeoHashGrid mockAggregation = createMockedGeoHashGridAggregation();
+
+        AggregationResource resource = assembler.toResource(mockAggregation);
+
+        assertEquals("locations", resource.getName());
+        assertThat(resource.getFacetValues(), hasSize(1));
+    }
+
+    private GeoHashGrid createMockedGeoHashGridAggregation() {
+        List<GeoHashGrid.Bucket> buckets = new ArrayList<>();
+        GeoHashGrid.Bucket mockBucket = mock(GeoHashGrid.Bucket.class);
+        when(mockBucket.getKeyAsGeoPoint()).thenReturn(new GeoPoint());
+        when(mockBucket.getDocCount()).thenReturn(1L);
+        buckets.add(mockBucket);
+
+        GeoHashGrid mockAggregation = mock(GeoHashGrid.class);
+        when(mockAggregation.getName()).thenReturn("locations");
+        when(mockAggregation.getBuckets()).thenReturn(buckets);
+        return mockAggregation;
     }
 }
